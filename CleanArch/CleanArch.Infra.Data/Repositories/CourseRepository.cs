430e42dab365be871b5245b910aff4f2b854e29d
sallysway <ioanaandreea476@gmail.com>
2021-05-17 16:09:24 +0300

    Implemented course service and course repository

---
+++ CleanArch/CleanArch.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data_.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx;
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
